43b81af3d8af29ce7d156c5a4981773fd567bd81
Xuancheng_Mao <121801880+oneTSR@users.noreply.github.com>
2023-09-03 19:58:18 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 map<long long,string> m{{0,"(1-1)*4514"},{1,"11/(45-1)*4"},{2,"-11+4-5+14"},{3,"11*(-4)+51-4"},{4,"-11-4+5+14"},{5,"11-4*5+14"},{6,"1-14+5+14"},{7,"11-4+5-1-4"},{8,"11-4+5/1-4"},{9,"11-4+5+1-4"},{10,"-11/4+51/4"}};
 void calc(long long num){
-    /*if(num%114514==0){
-        for(long long i=0;i<=num%114514;i++){
+    if(num%114514==0){
+        for(long long i=1;i<=num/114514;i++){
         	cout<<114514;
-			if(i<num%114514-1) cout<<"+";
+			if(i<num/114514-1) cout<<"+";
 		}
 		cout<<endl; 
         return;
-    }*/  
+    }
     if(num<=10){
         cout<<m[num]<<endl;
         return;
